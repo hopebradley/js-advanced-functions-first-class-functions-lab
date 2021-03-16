cb6dc5586257526c98753313f11dcdebd27dbcd8
Hope Bradley <hope.e.bradley@gmail.com>
2021-03-16 15:17:21 -0400

    commit tree 8137bf1f824858ea7fd7b1eb3d46582ec0762781

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+  return function(fare) {
+    return fare*num;
+  }
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(drivers, firstLast) {
+  return firstLast(drivers);
+}
